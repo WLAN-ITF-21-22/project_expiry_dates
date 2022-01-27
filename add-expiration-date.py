fc7e1650f7c153d5b0efffdc1a735dbe3b3a5bf2
Lander Wuyts <lander.wuyts@student.thomasmore.be>
2022-01-27 09:51:13 +0100

    retrieve id from products table

--- add-expiration-date.py
+++ add-expiration-date.py
@@ -48,7 +48,21 @@ db_cursor = mydb.cursor()
 ### FUNCTIONS ###
 #################
 
+def print_list(list):
+    """
+    A function for printing items in a list. Useful for testing.
+    Returns nothing.
+    """
+    for item in list:
+        print(item)
+
+
 def read_excel(excelsheet):
+    """
+    Reads a given excel sheet.
+    Only the last entry (not accounting for gaps) is read.
+    returns: a list containing the last entry's barcode, amount and expiration date
+    """
     # Start at 2nd place, 1st spot reserved for headers
     index = 2
     # search for latest entry
@@ -60,25 +74,38 @@ def read_excel(excelsheet):
     amount = excelsheet["B{}".format(index)].value
     expiration_date = excelsheet["C{}".format(index)].value
 
-    # do something with values        
-    print("Barcode: {}\nAmount: {}\nExpiration date: {}\n".format(barcode, amount, expiration_date))
+    # return values
+    return [barcode, amount, expiration_date]
+    
 
 
-def read_db(cursor):
+def find_id_db(cursor, barcode):
+    """
+    Given a cursor (link to mysql database) and a barcode,
+    this function returns the id if it exists in the database, 
+    else it returns None and prints an error message
+    """
     # Read data
-    cursor.execute("SELECT * FROM products")
+    cursor.execute("SELECT id, code, name \
+        FROM products \
+        WHERE code = {}".format(barcode))
     # Fetch data
     result = cursor.fetchall()
 
-    # do something with values
-    for line in result:
-        print(line)
+    if result == []:
+        # Return None and error message if the barcode is not in the database
+        message = "Barcode not in database. Add product to stock in uniCenta"
+        print(message)
+        return None
+    else:
+        # Return id
+        return result[0][0]
 
 
 #########################
 ### EXECUTION OF CODE ###
 #########################
 
-read_excel(sheet)
-
-read_db(db_cursor)
+scanned_product = read_excel(sheet)
+barcode_db_id = find_id_db(db_cursor, '054102282742300')
+print(barcode_db_id)
