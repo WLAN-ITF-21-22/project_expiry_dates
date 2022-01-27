952645e29940c93be16a5ec6ef56e02d26cf9dea
Lander Wuyts <lander.wuyts@student.thomasmore.be>
2022-01-27 10:47:41 +0100

    add-product script without practical testing

--- add-expiration-date.py
+++ add-expiration-date.py
@@ -57,41 +57,45 @@ def print_list(list):
         print(item)
 
 
-def read_excel(excelsheet):
+def read_excel():
     """
-    Reads a given excel sheet.
+    Reads the Excel sheet set earlier.
     Only the last entry (not accounting for gaps) is read.
-    returns: a list containing the last entry's barcode, amount and expiration date
+    Returns: a list containing the last entry's barcode, amount and expiration date
     """
+    # Global variables
+    global sheet
     # Start at 2nd place, 1st spot reserved for headers
     index = 2
     # search for latest entry
-    while excelsheet["A{}".format(index)].value != None:
+    while sheet["A{}".format(index)].value != None:
         index += 1
     index -= 1
     # get values
-    barcode = excelsheet["A{}".format(index)].value
-    amount = excelsheet["B{}".format(index)].value
-    expiration_date = excelsheet["C{}".format(index)].value
+    barcode = sheet["A{}".format(index)].value
+    amount = sheet["B{}".format(index)].value
+    expiration_date = sheet["C{}".format(index)].value
 
     # return values
     return [barcode, amount, expiration_date]
     
 
 
-def find_id_db(cursor, barcode):
+def find_id_db(barcode):
     """
-    Given a cursor (link to mysql database) and a barcode,
+    Given a barcode,
     this function returns the id if it exists in the database, 
     else it returns None and prints an error message
     """
+    # Global variables
+    global db_cursor
     # Read data
-    cursor.execute("SELECT id, code, name \
+    db_cursor.execute("SELECT id \
         FROM products \
         WHERE code = {}".format(barcode))
-    # Fetch data
-    result = cursor.fetchall()
+    result = db_cursor.fetchall()
 
+    # Check if the barcode is already in the database
     if result == []:
         # Return None and error message if the barcode is not in the database
         message = "Barcode not in database. Add product to stock in uniCenta"
@@ -102,10 +106,51 @@ def find_id_db(cursor, barcode):
         return result[0][0]
 
 
+def check_expiry_database(id, expiration_date):
+    """
+    Checks whether the product already exists within the expiration date database
+    returns: True or False
+    """
+    # Global variables
+    global db_cursor
+    # Check if the product with the expiration date already appears in the database
+    db_cursor.execute("SELECT * \
+        FROM expired\
+        WHERE id = {} AND vervaldatum = {}".format(id, expiration_date))
+    result = db_cursor.fetchall()
+    # If the product is not yet in the expired database
+    return result == []
+
+
+
+def write_entry_expiration_db(id, scanned_product):
+    """
+    Updates "expires" database
+    If the product with expiration date already appears, the product is updated
+    If the product with expiration date does not appear, the product is inserted
+    """
+    # Global variables
+    global db_cursor
+    # Variables to be inserted
+    amount = scanned_product[1]
+    expiration_date = scanned_product[2]
+    # Check if the product already exists in the database with this expiration date
+    if check_expiry_database(id, expiration_date):
+        # if the product is already in the database with the correct expiration date, update the amount
+        db_cursor.execute("UPDATE expired \
+            SET aantal += {}\
+            WHERE id = {} AND vervaldatum = {}".format(amount, id, expiration_date))
+    else:
+        # if the product is not yet in the database, add it
+        db_cursor.execute("INSERT INTO expired(id, aantal, vervaldatum)\
+            VALUES({}, {}, {})".format(id, amount, expiration_date))
+
+
 #########################
 ### EXECUTION OF CODE ###
 #########################
 
 scanned_product = read_excel(sheet)
-barcode_db_id = find_id_db(db_cursor, '054102282742300')
-print(barcode_db_id)
+# barcode_db_id = find_id_db(scanned_product)
+barcode_db_id = find_id_db('05410228274230')
+write_entry_expiration_db(barcode_db_id, scanned_product)
